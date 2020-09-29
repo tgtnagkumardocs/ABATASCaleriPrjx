d5ccfca0108dca3c9607bc5a7be10afd76009711
NageshKumar <tgtnagkumardocs@gmail.com>
2020-09-29 11:14:22 +0530

    2nd commit

--- AmicorpPrjxs/Controllers/ValuesController.cs
+++ AmicorpPrjxs/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace AmicorpPrjxs.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2m" };
         }
 
         // GET api/values/5
